c616dcd327a7a4327095418bc469e81fa7a6f106
e2toe8 <polgant1015@gmail.com>
2026-01-21 16:59:48 +0900

    [add] session for login

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 
 accounts = {
@@ -10,29 +12,41 @@ accounts = {
     'test': 'test',
 }
 
-@app.route('/', methods=['GET'])
-def get_home():
+
+@app.route('/login', methods=['GET'])
+def get_login():
+    if 'username' in session:
+        return redirect(url_for('get_login_success'))
+
     return render_template('login.html')
 
 
 @app.route('/login', methods=['POST'])
 def post_login():
+    if 'username' in session:
+        return redirect(url_for('get_login_success'))
+
     username = request.form.get('username')
     password = request.form.get('password')
     if username in accounts and accounts[username] == password:
+        session['username'] = username
         return redirect(url_for('get_login_success'))
     else:
-        return redirect(url_for('get_login_failure'))
+        return redirect(url_for('get_login_fail'))
 
 
-@app.route('/login_success')
+@app.route('/login-success')
 def get_login_success():
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
     return render_template('login_success.html')
 
 
-@app.route('/login_failure')
-def get_login_failure():
-    return render_template('login_failure.html')
+@app.route('/login-fail')
+def get_login_fail():
+    if 'username' in session:
+        return redirect(url_for('get_login_success'))
+    return render_template('login_fail.html')
 
 
 if __name__ == '__main__':
